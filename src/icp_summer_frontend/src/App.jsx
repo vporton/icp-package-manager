130be240dd36f7e8dcf92179fe894c9f8b229d75
Victor Porton <porton.victor@gmail.com>
2024-07-10 11:00:07 +0100

    navigable UI

--- src/icp_summer_frontend/src/App.jsx
+++ src/icp_summer_frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import { Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { package_manager } from '../../declarations/package_manager';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
 const packagesToRepair = [ // TODO
   {installationId: 3, name: "fineedit", version: "2.3.5"}
 ]
 
 function App() {
+  return <BrowserRouter><App2/></BrowserRouter>;
+}
+
+function App2() {
   const [greeting, setGreeting] = useState('');
 
   function handleSubmit(event) {
@@ -19,6 +24,8 @@ function App() {
     return false;
   }
 
+  const navigate = useNavigate();
+
   return (
     <main id="main">
       <p style={{textAlign: 'center'}}>
@@ -29,33 +36,44 @@ function App() {
           <nav>
             <Navbar className="bg-body-secondary" style={{width: "auto"}}>
               <Nav>
+                <Nav.Link onClick={() => navigate("/")}>Main page</Nav.Link>{" "}
+              </Nav>
+              <Nav>
                 <Nav.Link onClick={() => navigate("/installed")}>Installed packages</Nav.Link>{" "}
               </Nav>
             </Navbar>
           </nav>
-          <form action="#" onSubmit={handleSubmit}>
-            <label htmlFor="name">Enter package name to install:</label>{" "}
-            <input id="name" alt="Name" type="text" />
-            <button type="submit">Start installation</button>
-          </form>
-          {packagesToRepair.length !== 0 ?
-            <>
-              <h2>Partially Installed</h2>
-              <ul>
-                {packagesToRepair.map(p =>
-                  <li key={p.installationId}>
-                    <input type='checkbox'/>{" "}
-                    {p.name} {p.version}
-                  </li>
-                )}
-              </ul>
-              <p><button>Install checked</button> <button>Delete checked</button></p>
-            </>
-          : ""}
+          <Routes> {/* TODO: Refactor into sub-components. */}
+            <Route path="/" element={
+              <div>
+                <form action="#" onSubmit={handleSubmit}>
+                  <label htmlFor="name">Enter package name to install:</label>{" "}
+                  <input id="name" alt="Name" type="text" />
+                  <button type="submit">Start installation</button>
+                </form>
+                {packagesToRepair.length !== 0 ?
+                  <>
+                    <h2>Partially Installed</h2>
+                    <ul>
+                      {packagesToRepair.map(p =>
+                        <li key={p.installationId}>
+                          <input type='checkbox'/>{" "}
+                          <code>{p.name}</code> {p.version}
+                        </li>
+                      )}
+                    </ul>
+                    <p><button>Install checked</button> <button>Delete checked</button></p>
+                  </>
+                  : ""}
+                </div>}/>
+              <Route path="/installed" element={
+                <ul><code>photoedit</code> 3.5.6 <button>uninstall  </button></ul>
+              }/>
+            </Routes>
         </Container>
       </div>
     </main>
-  );
+ );
 }
 
 export default App;
