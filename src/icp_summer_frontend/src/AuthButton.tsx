e66ce58eb4035995ccbb640bcd3678f838c1f0fb
Victor Porton <porton.victor@gmail.com>
2024-07-10 20:14:57 +0100

    display user's principal

--- src/icp_summer_frontend/src/AuthButton.tsx
+++ src/icp_summer_frontend/src/AuthButton.tsx
@@ -1,13 +1,18 @@
 import Button from 'react-bootstrap/Button';
 
 import { useInternetIdentity } from '@identity-labs/react-ic-ii-auth';
+import DisplayPrincipal from './DisplayPrincipal';
 
 export const AuthButton = () => {
   const { authenticate, signout, isAuthenticated, identity } = useInternetIdentity()
   console.log('>> initialize your actors with', { identity })
   return (
-    <Button onClick={isAuthenticated ? signout : authenticate}>
-      {isAuthenticated ? 'Logout' : 'Login'}
-    </Button>
-  )
+    <span>
+      <Button onClick={isAuthenticated ? signout : authenticate}>
+        {isAuthenticated ? 'Logout' : 'Login'}
+      </Button>
+      {" "}
+      <DisplayPrincipal value={identity?.getPrincipal() ?? undefined}/>
+    </span>
+  );
 }
