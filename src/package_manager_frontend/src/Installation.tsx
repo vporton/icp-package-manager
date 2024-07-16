17738416a83c6fdedb48d05279d790f1109be46e
Victor Porton <porton.victor@gmail.com>
2024-07-16 17:49:27 +0300

    show more package info

--- src/package_manager_frontend/src/Installation.tsx
+++ src/package_manager_frontend/src/Installation.tsx
@@ -4,17 +4,26 @@ import { useEffect, useState } from "react";
 import { package_manager } from "../../declarations/package_manager";
 import { InstalledPackageInfo } from "../../declarations/package_manager/package_manager.did";
 import Button from "react-bootstrap/esm/Button";
+import { FullPackageInfo, PackageInfo, RealPackageInfo, RepositoryPartition, idlFactory as repositoryPartitionIDL } from '../../declarations/RepositoryPartition/RepositoryPartition.did.js';
+import { Actor } from "@dfinity/agent";
 
 export default function Installation(props: {}) {
     const { installationId } = useParams();
     const {defaultAgent} = useAuth();
     const [pkg, setPkg] = useState<InstalledPackageInfo | undefined>();
+    const [pkg2, setPkg2] = useState<PackageInfo | undefined>();
     useEffect(() => {
         if (defaultAgent === undefined) {
             return;
         }
         package_manager.getInstalledPackage(BigInt(installationId!)).then(pkg => {
             setPkg(pkg);
+            const part: RepositoryPartition = Actor.createActor(repositoryPartitionIDL, {canisterId: pkg.packageCanister!, agent: defaultAgent});
+            part.getFullPackageInfo(pkg.name).then(fullInfo => {
+                const pi = fullInfo.packages.filter(([version, _]) => version == pkg.version).map(([version, pkg]) => pkg)[0]; // TODO: undefined
+                setPkg2(pi);
+            });
+
         });
     }, [defaultAgent]);
 
@@ -31,6 +40,11 @@ export default function Installation(props: {}) {
             <p><strong>Installation ID:</strong> {installationId}</p>
             <p><strong>Package name:</strong> {pkg?.name}</p>
             <p><strong>Package version:</strong> {pkg?.version}</p>
+            <p><strong>Short description:</strong> {pkg2?.base.shortDescription}</p>
+            <p><strong>Long description:</strong> {pkg2?.base.longDescription}</p>
+            { (pkg2?.specific as { real: RealPackageInfo }).real && 
+                <p><strong>Dependencies:</strong> {(pkg2?.specific as { real: RealPackageInfo }).real.dependencies.join(", ")}</p>
+            }
             {/* TODO: description is missing in `pkg`, why? */}
             <p><Button onClick={uninstall}>Uninstall</Button></p>
         </>
