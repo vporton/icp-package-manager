24f02e4789d705dc727047f692b42e6244e3fb52
Victor Porton <porton.victor@gmail.com>
2024-10-01 22:14:16 +0300

    design bug fix

--- src/package_manager_frontend/src/MainPage.tsx
+++ src/package_manager_frontend/src/MainPage.tsx
@@ -127,7 +127,7 @@ export default function MainPage() {
                 <>
                     <Alert variant="warning">This page was not bookmarked in our bookmark system.
                         You are <strong>strongly</strong> recommended to bookmark it, otherwise
-                        you may lose this URL and be unable to find it.
+                        you may lose this URL and be unable to find it.{" "}
                         <a className="btn btn-primary" href={bookmarkingUrl} target="_blank">Bookmark</a>
                     </Alert>
                     <Alert variant="info">If you lose the URL, you can find it at the bootstrapper site.</Alert>
