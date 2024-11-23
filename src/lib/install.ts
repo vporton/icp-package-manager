de54b92c43be3c15e0055001a1815ae539ef7cd0
Victor Porton <porton.victor@gmail.com>
2024-11-23 19:22:38 +0200

    removed unused imports

---
+++ src/lib/install.ts
@@ -0,0 +1,11 @@
+// FIXME: Use.
+async function installPackageWithModules({
+    packageName: PackageName;
+    repo: Principal;
+    user: Principal;
+    version: Version;
+}): {
+    installationId: InstallationId;
+} {
+
+}
