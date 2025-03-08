ed739dc0e3b8ef2dec962e9a59b25620900be36b
Victor Porton <porton.victor@gmail.com>
2025-03-08 05:05:58 +0200

    more tracing

--- src/lib/install.ts
+++ src/lib/install.ts
@@ -133,7 +133,9 @@ export async function waitTillInitialized(agent: Agent, package_manager: Princip
                 resolve();
                 return;
             }
-            catch (_) {} // TODO
+            catch (e) {
+                console.log("Waiting for initialization: " + (e as any).message); // TODO: shorter message
+            }
             if (i == 30) {
                 reject("Cannot initilialize canisters");
             }
