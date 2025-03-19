163ccef8c097f7c41588907ea25a04a9ba1ef460
Victor Porton <porton.victor@gmail.com>
2025-03-20 00:10:47 +0200

    remove superfluous design

--- src/package_manager_frontend/src/App.tsx
+++ src/package_manager_frontend/src/App.tsx
@@ -200,11 +200,11 @@ function App2() {
               </Nav>
               <Nav style={{display: isAuthenticated ? undefined : 'none'}}>
                 <Dropdown>
-                  <Dropdown.Toggle  >
+                  <Dropdown.Toggle>
                     Cycles balance: {cyclesAmount !== undefined ? `${String(cyclesAmount/10**12)}T` : "Loading..."}{" "}
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
-                    <Dropdown.Item as="div" className="dropdown-item">
+                    <Dropdown.Item as="div">
                       <AddressPopup/>
                     </Dropdown.Item>
                   </Dropdown.Menu>
