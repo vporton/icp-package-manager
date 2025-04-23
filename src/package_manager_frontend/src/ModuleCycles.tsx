6ed7aca359cb1ad1a6c4d360f55f920f31a90c83
Victor Porton <porton.victor@gmail.com>
2025-04-23 17:14:38 +0300

    add output modules tree

--- src/package_manager_frontend/src/ModuleCycles.tsx
+++ src/package_manager_frontend/src/ModuleCycles.tsx
@@ -9,37 +9,41 @@ import { URLSearchParams } from "url";
 export default function ModuleCycles() {
     const params = new URLSearchParams(window.location.search);
     const pmPrincipal = Principal.fromText(params.get('_pm_pkg0.backend')!);
-    const { agent } = useAuth();
+    const { agent, isAuthenticated } = useAuth();
     type Module = {
         moduleName: string;
         cycles: bigint | undefined;
     };
     const [pkgs, setPkgs] = useState<{packageName: string, packageVersion: string, modules: Module[]}[]>([]);
     useEffect(() => {
-        const _pkgs: {packageName: string, packageVersion: string, modules: Module[]}[] = [];
-        const packageManager: PackageManager = createPackageManager(pmPrincipal, {agent});
-        packageManager.getAllInstalledPackages().then(packages => {
-            for (const p of packages) {
-                if ((p[1].package.specific as any).real) {
-                    const real = (p[1].package.specific as any).real as SharedInstalledPackageInfo;
-                    _pkgs.push({
-                        packageName: p[1].package.base.name,
-                        packageVersion: p[1].package.base.version,
-                        // TODO@P3: `additionalModules`
-                        modules: real.modulesInstalledByDefault.map(m => {
-                            return {
-                                moduleName: m[0],
-                                cycles: undefined,
-                            }
-                        }),
-                    });
+        if (isAuthenticated) {
+            const _pkgs: {packageName: string, packageVersion: string, modules: Module[]}[] = [];
+            const packageManager: PackageManager = createPackageManager(pmPrincipal, {agent});
+            packageManager.getAllInstalledPackages().then(packages => {
+                for (const p of packages) {
+                    if ((p[1].package.specific as any).real) {
+                        const h = {
+                            packageName: p[1].package.base.name,
+                            packageVersion: p[1].package.base.version,
+                            // TODO@P3: `additionalModules`
+                            modules: p[1].modulesInstalledByDefault.map(m => {
+                                return {
+                                    moduleName: m[0],
+                                    cycles: undefined,
+                                }
+                            }),
+                        };
+                        _pkgs.push(h);
+                    }
                 }
-            }
-            setPkgs(_pkgs);
-        });
+                setPkgs(_pkgs);
+            });
+        } else {
+            setPkgs([]);
+        }
 
         // Get ICRC1 cycles for each module:
-    }, []);
+    }, [agent, isAuthenticated]);
     return (
         <>
             <h2>Modules Cycles</h2>
