d0da44d4c0a07ca4b11f4306bddff6c86567b7ad
mathematician <porton@narod.ru>
2025-06-22 23:32:53 +0300

    fix browser process.version

--- src/package_manager_frontend/src/main.tsx
+++ src/package_manager_frontend/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
+// readable-stream expects `process.version` to exist, but browser polyfills may
+// omit it. Avoid runtime errors by providing a default value if missing.
+if (typeof process !== 'undefined' && typeof (process as any).version !== 'string') {
+  (process as any).version = '';
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
