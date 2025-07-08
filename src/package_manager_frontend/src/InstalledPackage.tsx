384103c125d59e20b0247b8dfae12f614d4a4663
Victor Porton <porton.victor@gmail.com>
2025-07-08 04:00:08 +0300

    add FIXME comment

--- src/package_manager_frontend/src/InstalledPackage.tsx
+++ src/package_manager_frontend/src/InstalledPackage.tsx
@@ -112,6 +112,7 @@ export default function InstalledPackage(props: {}) {
     const usedCycles = searchParams.get('usedCycles');
 
     return (
+        // FIXME@P1: Remove `XXX` below.
         <>
         XXX
             {usedCycles && <Alert variant="info">Installation used {Number(usedCycles) / 10**12}T cycles.</Alert>}
